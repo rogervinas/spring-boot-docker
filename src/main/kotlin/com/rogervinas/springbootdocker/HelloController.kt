a720d294325c6bcef1a437cb5422c06c0bfd9834
Roger Viñas <roger.vinas@adevinta.com>
2021-05-17 18:56:17 +0200

    Add HelloController and test

---
+++ src/main/kotlin/com/rogervinas/springbootdocker/HelloController.kt
@@ -0,0 +1,11 @@
+package com.rogervinas.springbootdocker
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello() = "hello!"
+}
--- src/test/kotlin/com/rogervinas/springbootdocker/ApplicationTests.kt
+++ src/test/kotlin/com/rogervinas/springbootdocker/ApplicationTests.kt
@@ -1,12 +1,27 @@
 package com.rogervinas.springbootdocker
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.web.reactive.function.client.WebClient
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @LocalServerPort
+    private var port: Int = 0
+
+    @Test
+    fun `should say hello`() {
+        val body = WebClient.builder()
+                .baseUrl("http://localhost:$port").build()
+                .get().uri("/hello")
+                .exchangeToMono {
+                    it.bodyToMono(String::class.java)
+                }.block()
+
+        assertThat(body).isEqualTo("hello!")
+    }
 }
